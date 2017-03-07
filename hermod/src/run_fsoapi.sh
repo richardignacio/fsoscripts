51f633eea65d1d037cae6f183b3896eb0ebca2b9
Richard Ignacio <richard.ignacio@gmail.com>
2017-03-06 20:45:09 -0700

    Created deployment script and updated README

--- hermod/src/run_fsoapi.sh
+++ hermod/src/run_fsoapi.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source /opt/fireeye/fso/config/iso_package_dev_env
 rm -f hermod.log*
+kill `ps aux | grep hermod | grep -v grep | tr -s " " | cut -d ' ' -f 2`
 nohup /opt/fireeye/fso/apps/engine/python/global/bin/python hermod.py > /dev/null 2>&1 &
